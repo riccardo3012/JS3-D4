5e8f9ed3c2552b78a9ab010b5ab4e5d99127517b
riccardo3012 <ronca.rcr@gmail.com>
2023-08-24 22:12:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+//rzjlEJDO5Ge9VV1iiKImqIbVCR25JwaBi7XHoQCPD4XFNcfvujlnOutZ
+const apiKey = "rzjlEJDO5Ge9VV1iiKImqIbVCR25JwaBi7XHoQCPD4XFNcfvujlnOutZ";
+const link = "https://api.pexels.com/v1/search?query=[your-query]";
+const linkDue = "https://api.pexels.com/v1/search?query=[your-secondary-query]";
